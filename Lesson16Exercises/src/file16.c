882ecd8a3b473d48581ee3e83c882b372cc5975d
Daulet <daulet.amirkhanov.official@gmail.com>
2017-10-14 13:13:01 +0600

    Lesson 16

---
+++ Lesson16Exercises/src/file16.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <math.h>
+
+double expApprox(double, int);
+
+int nFactorial(int);
+
+double sinApprox(double, int);
+
+int main() {
+
+//	expApprox();
+
+	printf("%lf\n", expApprox(1,30));
+
+	printf("%lf\n", sinApprox(1.6,6));
+
+	return 0;
+
+}
+
+double expApprox(double x, int n) {
+
+	double sum = 0;
+
+	for (int i = 0; i<= n; i++) {
+		sum += (pow(x,i)/nFactorial(i));
+	}
+
+	return sum;
+}
+
+
+double sinApprox(double x, int n) {
+
+	double sum = 0;
+
+	int j = 0;
+
+//	x *= 180/M_PI;
+
+	for (int i = 1; i<=n; i+=2, j++) {
+		if(j%2 == 0){
+//			printf("+");
+			sum += (pow(x,i)/nFactorial(i));
+			continue;
+		}
+		sum -= (pow(x,i)/nFactorial(i));
+//		printf("-");
+	}
+
+	return sum;
+}
+
+
+int nFactorial(int n) {
+
+	if (n == 1 || n == 0) return 1;
+
+	for (int i = n-1; i>0; i--) {
+		n *= i;
+	}
+
+	return n;
+}
